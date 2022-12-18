59716ba0240c79ac6b393bff6843f668d957fffe
Svetlana4278Zh <svetlana.zh@inbox.ru>
2022-12-18 22:13:50 +0300

    Homework 14.12

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -23,5 +25,23 @@ public class Book {
 
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return '"' + title + '"' + " - " + author + " - " + yearOfPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearOfPublication);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,9 @@ public class Main {
         Author pushkin = new Author("Александр", "Пушкин");
         Book mumu = new Book("Муму", turgenev, 1854);
         Book onegin = new Book("Евгений Онегин", pushkin, 1833);
-        printBook(mumu);
-        printBook(onegin);
+        System.out.println(mumu);
+        System.out.println(onegin);
         onegin.setYearOfPublication(1837);
-        printBook(onegin);
-    }
-
-    public static void printBook(Book book){
-        System.out.print(book.getTitle() + " - ");
-        System.out.print(book.getAuthor().getName() + " "+ book.getAuthor().getLastName()+ " - ");
-        System.out.println(book.getYearOfPublication());
+        System.out.println(onegin);
     }
 }
